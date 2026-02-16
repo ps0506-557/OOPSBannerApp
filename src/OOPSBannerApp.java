d7ab11d9c3e47067bf2cd5f77856684f5dc96fff
Praveen <sakhamuri.praveen2007@gmail.com>
2026-02-17 02:26:03 +0530

    Initial commit: project setup

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPSBannerApp UC1 – OOPS Banner Display Application
+ *
+ * Displays OOPS acronym to the console.
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
